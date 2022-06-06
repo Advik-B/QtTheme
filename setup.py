6e322d967164385fe87b718cd7128cf2a8ba557e
Advik <advik.b@gmail.com>
2022-06-06 20:15:34 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="QtTheme",
+    version="1.0.0",
+    author="Advik",
+    author_email="advik.b@gmail.com",
+    description="Themes for Qt",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Advik-B/QtTheme",
+    project_urls={
+        "Bug Tracker": "https://github.com/Advik-B/QtTheme/issues/new/choose",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": ".", "themes": "themes"},
+    python_requires=">=3.9",
+)
